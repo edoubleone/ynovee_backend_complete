0ddc6b51edb01a9dbf6a1df2dfa5011bea7c18b4
parkashsharma01@gmail.com <PSgithub@123#>
2023-12-09 09:05:41 +0530

    Update Setting File

--- roadersmap/settings.py
+++ roadersmap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_u1(rozc0a+)(e@r#x!7^zqxu0-u4o2*p&-fesw8oabd&88&xw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["staging.roadersmap.com", "localhost", "127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
